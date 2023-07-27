23b1803cb131122c0cb6873acd1c63b22bc82b76
kushal <gkushalg01@gmail.com>
2023-07-27 23:43:22 +0530

    Working Better Now

--- run_ir_turtle.sh
+++ run_ir_turtle.sh
@@ -3,7 +3,7 @@
 if [ -e /dev/ttyACM0 ]
 then
     bash -c "sudo chmod a+rw /dev/ttyACM0"
-    bash -c "source /home/$USER/Documents/ir-turtle/catkin_ws/devel/setup.sh && roslaunch ir_turtle irturtle.launch"
+    bash -c "source `pwd`/catkin_ws/devel/setup.sh && roslaunch ir_turtle irturtle.launch"
 else
     echo "Arduino is not connected. Please connect arduino and try again."
 fi
