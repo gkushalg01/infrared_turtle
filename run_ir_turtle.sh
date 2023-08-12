ce6ba5556245012be4b207fc7cb8615b7218af14
Kushal Ghosh <gkushalg01@gmail.com>
2023-08-12 13:20:50 +0530

    Name fixed

--- run_ir_turtle.sh
+++ run_ir_turtle.sh
@@ -3,7 +3,7 @@
 if [ -e /dev/ttyACM0 ]
 then
     bash -c "sudo chmod a+rw /dev/ttyACM0"
-    bash -c "source `pwd`/catkin_ws/devel/setup.sh && roslaunch ir_turtle irturtle.launch"
+    bash -c "source `pwd`/catkin_ws/devel/setup.sh && roslaunch ir_turtle ir_turtle.launch"
 else
     echo "Arduino is not connected. Please connect arduino and try again."
 fi
